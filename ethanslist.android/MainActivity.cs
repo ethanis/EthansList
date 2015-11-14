a6436066369455ebce802046b72b487930141d61
Ethan Dennis <erdennis13@gmail.com>
2015-11-13 16:51:39 -0800

    First working model of android drawer view

--- ethanslist.android/MainActivity.cs
+++ ethanslist.android/MainActivity.cs
@@ -9,46 +9,105 @@ using Android.OS;
 using System.Collections.Generic;
 using System.Linq;
 using EthansList.Shared;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
 
 namespace ethanslist.android
 {
     [Activity(Label = "ethanlist.android", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        AvailableLocations locations;
-        ListView cityPickerListView;
-        ListView statePickerListView;
-        CityListAdapter cityAdapter;
-        StateListAdapter stateAdapter;
-        String state;
+        DrawerLayout          drawerLayout;
+        ActionBarDrawerToggle drawerToggle;
+        ListView              drawerListView;
+
+        Android.App.Fragment[] fragments = new Android.App.Fragment[] { new SelectCityFragment()};
+        string  [] titles    = new string  [] {    "Select City" };
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            locations = new AvailableLocations();
-            state = locations.States.ElementAt(0);
             SetContentView(Resource.Layout.Main);
 
-            cityPickerListView = FindViewById<ListView>(Resource.Id.cityPickerListView);
-            statePickerListView = FindViewById<ListView>(Resource.Id.statePickerListView);
+            //
+            // Retrieve the DrawerLayout from the XML
+            //
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawerLayout);
 
-            cityAdapter = new CityListAdapter(this, locations.PotentialLocations.Where(loc => loc.State == state));
-            stateAdapter = new StateListAdapter(this, locations.States);
+            //
+            // Create an instance of ActionBarDrawerToggle
+            //
+            drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, Resource.String.DrawerOpenDescription, Resource.String.DrawerCloseDescription);
 
-            cityPickerListView.Adapter = cityAdapter;
-            statePickerListView.Adapter = stateAdapter;
+            //
+            // Set the ActionBarDrawerToggle as a DrawerListener on the DrawerLayout so it receives drawer state-change callbacks
+            //
+            drawerLayout.SetDrawerListener(drawerToggle);
 
-            statePickerListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => {
-                state = locations.States.ElementAt(e.Position);
-                cityAdapter = new CityListAdapter(this, locations.PotentialLocations.Where(loc => loc.State == state));
-                cityPickerListView.Adapter = cityAdapter;
-            };
+            // 
+            // Must up-enable the home button, the ActionBarDrawerToggle will change the icon to the "hamburger"
+            //
+            ActionBar.SetDisplayHomeAsUpEnabled(true); 
 
-            cityPickerListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => {
-                var intent = new Intent(this, typeof(SearchActivity));
-                intent.PutExtra("city", locations.PotentialLocations.Where(loc => loc.State == state).ElementAt(e.Position).SiteName);
-                StartActivity(intent);
-            };
+            //
+            // Prepare the ListView that will serve as the menu
+            //
+            drawerListView = FindViewById<ListView>(Resource.Id.drawerListView);
+            drawerListView.Adapter = new ArrayAdapter<string>(this, Resource.Layout.ListViewMenuRow, Resource.Id.menuRowTextView, titles);
+            drawerListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => OnMenuItemClick(e.Position);
+            drawerListView.SetItemChecked(0, true); // Highlight the first item at startup
+            OnMenuItemClick(0);                     // Load Fragment 0 at startup
+        }
+
+        protected override void OnPostCreate(Bundle savedInstanceState)
+        {
+            //
+            // Initialization and any needed Restore operation are now complete.
+            // Sync the state of the ActionBarDrawerToggle to the drawer (i.e. show the "hamburger" if the drawer is closed or an arrow if it is open).
+            //
+            drawerToggle.SyncState();
+
+            base.OnPostCreate(savedInstanceState);
+        }
+
+        void OnMenuItemClick(int position)
+        {
+            //
+            // Show the selected Fragment to the user
+            //
+            base.FragmentManager.BeginTransaction().Replace(Resource.Id.frameLayout, fragments[position]).Commit();
+
+            //
+            // Update the Activity title in the ActionBar
+            //
+            this.Title = titles[position];
+
+            //
+            // Close the drawer
+            //
+            drawerLayout.CloseDrawer(drawerListView);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            //
+            // Forward all ActionBar-clicks to the ActionBarToggleButton.
+            // It will verify the click was on the "Home" button (i.e. the button at the left edge of the ActionBar).
+            // If so, it will toggle the state of the drawer. It will then return "true" so you know you do not need to do any more processing.
+            //
+            if (drawerToggle.OnOptionsItemSelected(item))
+                return true;
+
+            //
+            // Other cases go here for other buttons in the ActionBar.
+            // This sample app has no other buttons. This code is a placeholder to show what would be needed if there were other buttons.
+            //
+            switch (item.ItemId)
+            {
+                default: break;
+            }
+
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
---
+++ ethanslist.android/SelectCityFragment.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using EthansList.Shared;
+
+namespace ethanslist.android
+{
+    public class SelectCityFragment : Fragment
+    {
+        AvailableLocations locations;
+        ListView cityPickerListView;
+        ListView statePickerListView;
+        CityListAdapter cityAdapter;
+        StateListAdapter stateAdapter;
+        String state;
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.SelectCity, container, false);
+
+            locations = new AvailableLocations();
+            state = locations.States.ElementAt(0);
+
+            cityPickerListView = view.FindViewById<ListView>(Resource.Id.cityPickerListView);
+            statePickerListView = view.FindViewById<ListView>(Resource.Id.statePickerListView);
+
+            cityAdapter = new CityListAdapter(this.Activity, locations.PotentialLocations.Where(loc => loc.State == state));
+            stateAdapter = new StateListAdapter(this.Activity, locations.States);
+
+            cityPickerListView.Adapter = cityAdapter;
+            statePickerListView.Adapter = stateAdapter;
+
+            statePickerListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => {
+                state = locations.States.ElementAt(e.Position);
+                cityAdapter = new CityListAdapter(this.Activity, locations.PotentialLocations.Where(loc => loc.State == state));
+                cityPickerListView.Adapter = cityAdapter;
+            };
+
+            cityPickerListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => {
+                var intent = new Intent(this.Activity, typeof(SearchActivity));
+                intent.PutExtra("city", locations.PotentialLocations.Where(loc => loc.State == state).ElementAt(e.Position).SiteName);
+                StartActivity(intent);
+            };
+
+            return view;
+        }
+    }
+}
+
