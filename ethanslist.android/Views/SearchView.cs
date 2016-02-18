baec0f94b86b9fd8c60318e1efb2947f08418843
Ethan Dennis <erdennis13@gmail.com>
2016-02-18 15:48:35 -0800

    Search View class now adds view size relative to parent

--- ethanslist.android/Views/SearchView.cs
+++ ethanslist.android/Views/SearchView.cs
@@ -17,34 +17,65 @@ namespace ethanslist.android
     public class SearchView : LinearLayout
     {
         Button proceedButton;
+        Button secondButton;
+        Context context;
+
         public SearchView(Context context)
             : base(context)
         {
-            Initialize(context);
+            this.context = context;
+            Initialize();
         }
 
         public SearchView(Context context, IAttributeSet attrs)
             : base(context, attrs)
         {
-            Initialize(context);
+            this.context = context;
+            Initialize();
         }
 
         public SearchView(Context context, IAttributeSet attrs, int defStyle)
             : base(context, attrs, defStyle)
         {
-            Initialize(context);
+            this.context = context;
+            Initialize();
         }
 
-        void Initialize(Context context)
+        void Initialize()
         {
             this.SetBackgroundColor(ColorScheme.Clouds);
+            this.Orientation = Orientation.Vertical;
+            this.WeightSum = 1;
 
             proceedButton = new Button(context);
-            proceedButton.LayoutParameters = new ViewGroup.LayoutParams(WindowManagerLayoutParams.MatchParent, 100);
+            proceedButton.LayoutParameters = new LayoutParams(0, LayoutParams.WrapContent, 0.5f);
             proceedButton.SetBackgroundColor(ColorScheme.MidnightBlue);
             proceedButton.Text = "Search";
             proceedButton.SetTextColor(ColorScheme.Clouds);
-            this.AddView(proceedButton);
+
+            LinearLayout buttonLayout = RowHolder();
+            buttonLayout.AddView(proceedButton);
+            AddView(buttonLayout);
+
+            secondButton = new Button(context);
+            secondButton.LayoutParameters = new LayoutParams(0, LayoutParams.WrapContent, 0.75f);
+            secondButton.SetBackgroundColor(ColorScheme.Alizarin);
+            secondButton.Text = "Second";
+            secondButton.SetTextColor(ColorScheme.Clouds);
+
+            LinearLayout secondButtonLayout = RowHolder();
+            secondButtonLayout.AddView(secondButton);
+            AddView(secondButtonLayout);
+        }
+
+        private LinearLayout RowHolder()
+        {
+            LinearLayout layout = new LinearLayout(context);
+            layout.Orientation = Orientation.Horizontal;
+            layout.LayoutParameters = new ViewGroup.LayoutParams(LayoutParams.MatchParent, LayoutParams.WrapContent);
+            layout.WeightSum = 1;
+            layout.SetGravity(GravityFlags.CenterHorizontal);
+            return layout;
         }
     }
 }
