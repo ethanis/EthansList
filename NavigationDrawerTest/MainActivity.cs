fdd0d06988cf57ac1d45d7dadd911e51f3af8b72
Ethan Dennis <erdennis13@gmail.com>
2016-02-29 14:19:05 -0800

    Got material design theme of drawer layout initialized

---
+++ NavigationDrawerTest/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Support.Design.Widget;
+
+namespace EthansList.MaterialDroid
+{
+	[Activity(Label = "EthansList", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : AppCompatActivity
+	{
+		DrawerLayout drawerLayout;
+
+		protected override void OnCreate(Bundle bundle)
+		{			
+			base.OnCreate(bundle);
+
+			// Create UI
+			SetContentView(Resource.Layout.Main);
+			drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+			// Init toolbar
+			var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+			SetSupportActionBar(toolbar);	
+
+			// Attach item selected handler to navigation view
+			var navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+			navigationView.NavigationItemSelected += NavigationView_NavigationItemSelected;
+
+			// Create ActionBarDrawerToggle button and add it to the toolbar
+			var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.open_drawer, Resource.String.close_drawer);
+			drawerLayout.SetDrawerListener(drawerToggle);
+			drawerToggle.SyncState();
+		}
+
+		void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+		{
+            switch (e.MenuItem.ItemId)
+            {
+                case (Resource.Id.nav_home):
+                    // React on 'Home' selection
+                    break;
+                case (Resource.Id.nav_messages):
+                    // React on 'Messages' selection
+                    break;
+                case (Resource.Id.nav_friends):
+                    // React on 'Friends' selection
+                    break;
+                case (Resource.Id.nav_discussion):
+                    // React on 'Discussion' selection
+                    break;                
+            }
+
+            // Close drawer
+            drawerLayout.CloseDrawers();
+		}
+	}
+}
