6f3d0b5d6ab14670a84d68e3d96555501016a003
Ethan Dennis <erdennis13@gmail.com>
2016-03-02 09:48:01 -0800

    Added view to hold text and checkboxes within combopicker dialog

--- NavigationDrawerTest/Adapters/ComboPickerAdapter.cs
+++ NavigationDrawerTest/Adapters/ComboPickerAdapter.cs
@@ -43,7 +43,50 @@ namespace EthansList.MaterialDroid
 
         public override View GetView(int position, View convertView, ViewGroup parent)
         {
-            return new TextView(_context) { Text = _options[position] };
+            return new ComboRowView(_context, _options[position]);
+        }
+    }
+
+    public class ComboRowView : LinearLayout
+    {
+        readonly Context _context;
+
+        public ComboRowView(Context context, string title)
+            : base(context)
+        {
+            _context = context;
+            Initialize(title);
+        }
+
+        void Initialize(string title)
+        {
+            this.Orientation = Orientation.Horizontal;
+            this.WeightSum = 1;
+            this.LayoutParameters = new ListView.LayoutParams(LayoutParams.MatchParent, ConvertDpToPx(40));
+
+
+            var text = new TextView(_context) { Text = title };
+            text.SetTextSize(Android.Util.ComplexUnitType.Dip, 20);
+            text.LayoutParameters = new ViewGroup.LayoutParams(LayoutParams.WrapContent, LayoutParams.MatchParent);
+
+            AddView(text);
+
+            var checkBoxPlacer = new LinearLayout(_context);
+            checkBoxPlacer.LayoutParameters = new ViewGroup.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent);
+            checkBoxPlacer.SetBackgroundColor(Android.Graphics.Color.AliceBlue);
+            checkBoxPlacer.SetHorizontalGravity(GravityFlags.Right);
+
+            var checkbox = new CheckBox(_context);
+            checkbox.LayoutParameters = new ViewGroup.LayoutParams(LayoutParams.WrapContent, LayoutParams.MatchParent);
+            checkbox.SetPadding(0,0, ConvertDpToPx(15), 0);
+            checkBoxPlacer.AddView(checkbox);
+
+            AddView(checkBoxPlacer);
+        }
+
+        private int ConvertDpToPx(float dip)
+        {
+            return (int)(dip * _context.Resources.DisplayMetrics.Density);
         }
     }
 }
