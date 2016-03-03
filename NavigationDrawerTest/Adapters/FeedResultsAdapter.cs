5ae76c9a04f7cadcdfafc25e23af18ec63d510fa
Ethan Dennis <erdennis13@gmail.com>
2016-03-03 13:58:47 -0800

    Added base logic to house the feed results row in android

--- NavigationDrawerTest/Adapters/FeedResultsAdapter.cs
+++ NavigationDrawerTest/Adapters/FeedResultsAdapter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Android.App;
+using Android.Content;
 using Android.Views;
 using Android.Widget;
 using EthansList.Models;
@@ -42,7 +43,61 @@ namespace EthansList.MaterialDroid
 
         public override View GetView(int position, View convertView, ViewGroup parent)
         {
-            return new TextView(activity) { Text = postings[position].PostTitle };
+            var view = new FeedResultRow(activity, postings[position]);
+            //return new TextView(activity) { Text = postings[position].PostTitle };
+            string imageLink = postings[position].ImageLink;
+            if (imageLink != "-1")
+            {
+                Koush.UrlImageViewHelper.SetUrlDrawable(view._postingImage, imageLink, Resource.Drawable.placeholder);
+            }
+            else
+            {
+                view._postingImage.SetImageResource(Resource.Drawable.placeholder);
+            }
+
+            return view;
+        }
+    }
+
+    public class FeedResultRow : LinearLayout
+    {
+        readonly Context _context;
+        readonly Posting _posting;
+
+        public ImageView _postingImage { get; set; }
+        public TextView _postingTitle { get; set; }
+        public TextView _postingDescription { get; set; }
+
+        public FeedResultRow(Context context, Posting posting)
+            :base(context)
+        {
+            _context = context;
+            _posting = posting;
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            Orientation = Orientation.Horizontal;
+            WeightSum = 1;
+
+            var imageParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent);
+
+            var imageHolder = new LinearLayout(_context);
+            //imageHolder.LayoutParameters = new LayoutParams(0, ConvertDpToPx(100), 0.30f);
+            imageHolder.LayoutParameters = new LayoutParams(0, LayoutParams.WrapContent, 0.30f);
+
+            _postingImage = new ImageView(_context);
+            _postingImage.LayoutParameters = imageParams;
+            imageHolder.AddView(_postingImage);
+            imageHolder.SetBackgroundColor(Android.Graphics.Color.Blue);
+
+            AddView(imageHolder);
+        }
+
+        private int ConvertDpToPx(float dip)
+        {
+            return (int)(dip * _context.Resources.DisplayMetrics.Density);
         }
     }
 }
