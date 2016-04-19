8d85fb0a1e98fce6a694dafbe3a06345e59a0840
Ethan Dennis <erdennis13@gmail.com>
2016-04-19 15:36:09 -0700

    Modified iOS font theme to SF

--- EthansList.iOS/Helpers/Constants.cs
+++ EthansList.iOS/Helpers/Constants.cs
@@ -6,40 +6,45 @@ namespace ethanslist.ios
     public static class Constants
     {
         public static nfloat CityPickerRowHeight { get { return 40f; } }
-        public static string NormalFont {get{return "Helvetica";}}
-        public static string BoldFont {get{ return "Helvetica-Bold"; }}
-        public static string LightFont {get{ return "Helvetica-Light"; }}
+        public static string NormalFont { get { return "SanFranciscoDisplay-Regular"; } }
+        public static string BoldFont { get { return "SanFranciscoDisplay-Bold"; } }
+        public static string LightFont { get { return "SanFranciscoDisplay-Thin"; } }
 
-        public static UIStringAttributes LabelAttributes {get{ return att.lblAttributes;}}
-        public static UIStringAttributes ButtonAttributes {get{ return att.btnAttributes; }}
-        public static UIStringAttributes HeaderAttributes {get{ return att.headerAttributes; }}
-        public static UIStringAttributes CityPickerCellAttributes {get{return att.cityPickerCellAttributes;}}
-        public static UIStringAttributes FeedDescriptionAttributes {get{ return att.feedDescriptionAttributes; }}
-        public static int ButtonHeight {get{ return 36; }}
+        public static UIStringAttributes LabelAttributes { get { return att.lblAttributes; } }
+        public static UIStringAttributes ButtonAttributes { get { return att.btnAttributes; } }
+        public static UIStringAttributes HeaderAttributes { get { return att.headerAttributes; } }
+        public static UIStringAttributes CityPickerCellAttributes { get { return att.cityPickerCellAttributes; } }
+        public static UIStringAttributes FeedDescriptionAttributes { get { return att.feedDescriptionAttributes; } }
+        public static int ButtonHeight { get { return 36; } }
 
         static StringAttributes att = new StringAttributes();
     }
 
     public class StringAttributes
     {
-        public UIStringAttributes lblAttributes = new UIStringAttributes() {
+        public UIStringAttributes lblAttributes = new UIStringAttributes()
+        {
             Font = UIFont.FromName(Constants.NormalFont, 18f),
         };
 
-        public UIStringAttributes btnAttributes = new UIStringAttributes() {
+        public UIStringAttributes btnAttributes = new UIStringAttributes()
+        {
             Font = UIFont.FromName(Constants.LightFont, 18f),
             ForegroundColor = ColorScheme.Clouds
         };
 
-        public UIStringAttributes headerAttributes = new UIStringAttributes() {
-            Font = UIFont.FromName(Constants.BoldFont, 16f),  
+        public UIStringAttributes headerAttributes = new UIStringAttributes()
+        {
+            Font = UIFont.FromName(Constants.BoldFont, 16f),
         };
 
-        public UIStringAttributes cityPickerCellAttributes = new UIStringAttributes() {
+        public UIStringAttributes cityPickerCellAttributes = new UIStringAttributes()
+        {
             Font = UIFont.FromName(Constants.LightFont, 16f),
         };
 
-        public UIStringAttributes feedDescriptionAttributes = new UIStringAttributes() {
+        public UIStringAttributes feedDescriptionAttributes = new UIStringAttributes()
+        {
             Font = UIFont.FromName(Constants.LightFont, 14f)
         };
     }
